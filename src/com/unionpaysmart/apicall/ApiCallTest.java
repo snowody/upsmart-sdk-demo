a3e4290bbae92a4f4a06870532fe0f0712242a43
niexd <niexiongding@unionpaysmart.com>
2015-04-10 11:25:33 +0800

    fix conflict

--- src/com/unionpaysmart/apicall/ApiCallTest.java
+++ src/com/unionpaysmart/apicall/ApiCallTest.java
@@ -1,23 +1,10 @@
 package com.unionpaysmart.apicall;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import upsmart.data.sdk.api.DataApi;
 import upsmart.data.sdk.model.report.request.QueryNameRequest;
 import upsmart.data.sdk.model.report.response.QueryNameResponse;
 
 import com.alibaba.fastjson.JSON;
-=======
-=======
->>>>>>> demo_github/dev
-import com.alibaba.fastjson.JSON;
-import com.unionpaysmart.data.sdk.api.DataApi;
-import com.unionpaysmart.data.sdk.model.report.request.QueryNameRequest;
-import com.unionpaysmart.data.sdk.model.report.response.QueryNameResponse;
-<<<<<<< HEAD
->>>>>>> init project
-=======
->>>>>>> demo_github/dev
 
 /**
  *
@@ -53,14 +40,7 @@ public class ApiCallTest {
      * 初始化参数: 设置account和privateKey
      */
     public static void init() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         //DataApi.isTest = true; // isTest为true时调用测试环境，为false时调用生产环境
->>>>>>> init project
-=======
-        //DataApi.isTest = true; // isTest为true时调用测试环境，为false时调用生产环境
->>>>>>> demo_github/dev
         DataApi.account = "account"; // 设置account
         DataApi.privateKey = "6ad2693d06f9f75ceb3d41309f9e903c"; // 设置privateKey
     }
