f75c10b6b06b504fdca8c9e3f4d6f02ea4c80b39
niexd <niexiongding@unionpaysmart.com>
2015-04-10 10:51:27 +0800

    init project

--- src/com/unionpaysmart/apicall/ApiCallTest.java
+++ src/com/unionpaysmart/apicall/ApiCallTest.java
@@ -1,10 +1,17 @@
 package com.unionpaysmart.apicall;
 
+<<<<<<< HEAD
 import upsmart.data.sdk.api.DataApi;
 import upsmart.data.sdk.model.report.request.QueryNameRequest;
 import upsmart.data.sdk.model.report.response.QueryNameResponse;
 
 import com.alibaba.fastjson.JSON;
+=======
+import com.alibaba.fastjson.JSON;
+import com.unionpaysmart.data.sdk.api.DataApi;
+import com.unionpaysmart.data.sdk.model.report.request.QueryNameRequest;
+import com.unionpaysmart.data.sdk.model.report.response.QueryNameResponse;
+>>>>>>> init project
 
 /**
  *
@@ -40,6 +47,10 @@ public class ApiCallTest {
      * 初始化参数: 设置account和privateKey
      */
     public static void init() {
+<<<<<<< HEAD
+=======
+        //DataApi.isTest = true; // isTest为true时调用测试环境，为false时调用生产环境
+>>>>>>> init project
         DataApi.account = "account"; // 设置account
         DataApi.privateKey = "6ad2693d06f9f75ceb3d41309f9e903c"; // 设置privateKey
     }
