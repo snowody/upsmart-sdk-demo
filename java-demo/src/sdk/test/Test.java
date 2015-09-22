f930d01d7309afb5a15f54b8728f9aad849cbe46
Huang Kun <huangkun@unionpaysmart.com>
2015-09-22 17:59:21 +0800

    update demo

---
+++ java-demo/src/sdk/test/Test.java
@@ -0,0 +1,13 @@
+package sdk.test;
+
+/**
+ *
+ * @author Kevin Huang
+ * @version 0.0.3, 2015年9月22日 下午5:53:09
+ */
+public class Test {
+    public static void main(String[] args) {
+        ApiCallTest apiCallTest = new ApiCallTest();
+        apiCallTest.testQueryName();
+    }
+}
